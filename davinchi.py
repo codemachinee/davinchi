7773fdf3687550c823ca269162fdacf73849627a
codemachinee <johnybritva2@gmail.com>
2023-05-09 15:28:04 +0300

    api chat gpt в боте

--- davinchi.py
+++ davinchi.py
@@ -1,10 +1,34 @@
-import telebot
-from telebot import types
-from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
-from functions_file import value_plus_one, pstat, obnulenie_stat, ball_of_fate, Davinci, Artur
+from aiogram import Bot, executor, Dispatcher, types
+# from apscheduler.schedulers.background import BackgroundScheduler
+# from datetime import datetime
+from functions_file import Davinci
 from paswords import *
 
 token = lemonade
 
-bot = telebot.TeleBot(token)
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def command_start(message: types.Message):
+    await bot.send_message(message.chat.id, '''фрагмент в разработке
+
+/help - справка по боту''')
+
+
+@dp.message_handler(commands=['help'])
+async def command_start(message: types.Message):
+    await bot.send_message(message.chat.id, '''фрагмент в разработке
+
+/help - справка по боту''')
+
+
+@dp.message_handler(content_types='text')
+async def chek_message(v: types.Message):
+    b = str(v.text)
+    await Davinci(bot, v, b)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
--- functions_file.py
+++ functions_file.py
@@ -1,44 +1,39 @@
 # библиотека работы с гугл таблицами
-import gspread
+# import gspread
 import openai
 # библиотека проверки даты
-from datetime import datetime
+# from datetime import datetime
 from paswords import *
 
 saved_messages_davinci = []
-saved_messages_artur = []
 
 
-class Davinci:
-    global saved_messages_davinci
+async def Davinci(bot, message, text):
+    try:
+        openai.api_key = Davinci_key
+        answer_davinci = open('davinci.txt.txt', 'r', encoding='utf-8')
+        saved_messages_davinci.insert(0, f'Вы: {text}\n')
+        prompt_davinci = (str(answer_davinci.read()) + ''.join(reversed(saved_messages_davinci)))
 
-    def __init__(self, bot, message, text):
-        try:
-            self.bot = bot
-            self.message = message
-            self.text = text
-            openai.api_key = Davinci_key
-            answer_davinci = open('davinci.txt', 'r', encoding='utf-8')
-            saved_messages_davinci.insert(0, f'Вы: {self.text}\n')
-            prompt_davinci = (str(answer_davinci.read()) + ''.join(reversed(saved_messages_davinci)))
-            self.bot.send_message(message.chat.id, f'секунду..')
-
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=prompt_davinci,
-                temperature=0.3,
-                max_tokens=1000,
-                top_p=1,
-                frequency_penalty=0.0,
-                presence_penalty=0.0,
-            )
-            self.bot.send_message(message.chat.id, f'{response["choices"][0]["text"]}')
-            saved_messages_davinci.insert(0, f'{str(response["choices"][0]["text"])}\n')
-            if len(saved_messages_davinci) >= 8:
-                del saved_messages_davinci[3:]
-        except Exception:
-            self.bot.send_message(message.chat.id, "Простите но мне нужен перекур..")
-            del saved_messages_davinci[1:]
+        answer_message = await bot.send_message(message.chat.id, f'секунду..')
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt_davinci,
+            temperature=0.1,
+            max_tokens=1000,
+            top_p=1,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+        )
+      #  await bot.edit_message_text(message.chat.id, f'{response["choices"][0]["text"]}')
+        await bot.edit_message_text(f'{response["choices"][0]["text"]}', message.chat.id, answer_message.message_id)
+        saved_messages_davinci.insert(0, f'{str(response["choices"][0]["text"])}\n')
+        if len(saved_messages_davinci) >= 8:
+            del saved_messages_davinci[3:]
+    except Exception:
+        await bot.send_message(message.chat.id, "Простите но мне нужен перекур..")
+        await bot.send_message(admin_id, "Простите но мне нужен перекур..")
+        del saved_messages_davinci[1:]
 
 
 
